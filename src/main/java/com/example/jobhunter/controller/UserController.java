d7fa1c7877b54b89736ca5ed0f63651221428b6b
dangthaii <thaidv@hivetech.vn>
2024-06-05 14:14:52 +0700

    cấu hình cơ bản spring security

--- src/main/java/com/example/jobhunter/controller/UserController.java
+++ src/main/java/com/example/jobhunter/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.jobhunter.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +23,12 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/users")
 public class UserController {
   private final UserService userService;
+  private final PasswordEncoder passwordEncoder;
 
   @PostMapping
   public ResponseEntity<User> createUser(@RequestBody User user) {
+    var hashPassword = passwordEncoder.encode(user.getPassword());
+    user.setPassword(hashPassword);
     var newUser = userService.createUser(user);
     return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
   }
@@ -47,5 +51,12 @@ public class UserController {
   @GetMapping("/{id}")
   public User getUserById(@PathVariable Long id) {
     return userService.getUserById(id);
+  }
+
+  @GetMapping
+  public ResponseEntity<User> home() {
+    var newUser = new User();
+    newUser.setEmail("admin");
+    return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
   }
 }
