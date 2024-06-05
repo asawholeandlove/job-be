f3bf489d60d2558884abcd162e379d8357a3c77e
dangthaii <thaidv@hivetech.vn>
2024-06-05 11:16:19 +0700

    chuẩn hóa lại response trả về cho client

--- src/main/java/com/example/jobhunter/service/error/GlobalException.java
+++ src/main/java/com/example/jobhunter/service/error/GlobalException.java
@@ -1,13 +1,21 @@
 package com.example.jobhunter.service.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.jobhunter.model.RestResponse;
+
 @RestControllerAdvice
 public class GlobalException {
   @ExceptionHandler(IdInvalidException.class)
-  public ResponseEntity<String> handleIdInvalidException(IdInvalidException e) {
-    return ResponseEntity.badRequest().body(e.getMessage());
+  public ResponseEntity<RestResponse<Object>> handleIdInvalidException(IdInvalidException e) {
+    var res = new RestResponse<Object>();
+    res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+    res.setError("IdInvalidException");
+    res.setMessage(e.getMessage());
+
+    return ResponseEntity.badRequest().body(res);
   }
 }
